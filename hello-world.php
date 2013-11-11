2806bf3d43fa151e18a3327665861c8923ea4ed5
Artie Groove <apple2000@mail.ru>
2013-11-11 16:36:20 -0500

    initial step

---
+++ hello-world.php
@@ -0,0 +1,15 @@
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo 'Hello world from Cloud9!';
+/*
+$xmlreader = new XMLReader();
+if ( !$xmlreader->open("/file.xml") )
+    echo "<br />cannot open xml database";
+else echo 'strange, but it works )';
+*/
+
+echo '<br />' . date('Y-m-j H:i:s');
+?>
